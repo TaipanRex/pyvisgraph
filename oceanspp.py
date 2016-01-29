07a965b3a585a8cf400e266bd9680e6e7bf3156c
Christian Reksten-Monsen <christian@reksten-monsen.com>
2016-01-29 17:03:02 +0800

    Implemented partially working shortest path, but seems wrong on port (5,3) and crashes on (4.5,5). Ship at (10,5.5). Visibility test not implemented. Also added drawing of shortest path.

--- oceanspp.py
+++ oceanspp.py
@@ -35,6 +35,9 @@ class Graph:
             edges.append((vertex, v))
         return edges
 
+    def remove_edge(self, vertex, edge):
+        self.__graph_dict[vertex].remove(edge[1])
+
     def __str__(self):
         res = "vertices: "
         for vertex in self.__graph_dict:
@@ -54,23 +57,48 @@ def shortest_path(graph, ship, port):
     distance = {v:9999 for v in not_visited}
     distance[port] = 0
 
+    #Calculate distances
     v = None
+    ship_edges = []
     while v != ship:
-        min_value = min(distance.itervalues())
-        v = [key for key, value in distance.iteritems() if value == min_value].pop()
+        min_value = 9999
+        for d in not_visited:
+            if distance[d] < min_value:
+                min_value = distance[d]
+                v = d
         visited.append(v)
         not_visited.remove(v)
         if distance[v] + edge_distance(v, ship) < distance[ship]:
             # if visibility between node v and ship:
             if v == (8.0, 6.0) or v == (10.0, 1.5):
-                graph.add_edge([v, ship])
+                graph.add_edge((v, ship))
+                ship_edges.append((ship, v))
             for edge in graph.vertex_edges(v):
                 v2 = edge[1]
                 if distance[v2] > distance[v] + edge_distance(v, v2):
                     distance[v2] = distance[v] + edge_distance(v, v2)
-        break
-    print distance
-                
+
+    #Find the shortest path
+    path = [ship]
+    min_value = 9999
+    v = None
+    for edge in ship_edges:
+        if distance[edge[1]] < min_value:
+            min_value = distance[edge[1]]
+            v = edge[1]
+    path.append(v)
+
+    while v != port:
+        min_value = 9999
+        min_edge = None
+        for edge in graph.vertex_edges(v):
+            if distance[edge[1]] < min_value:
+                min_value = distance[edge[1]]
+                min_edge = edge
+        graph.remove_edge(min_edge[1], (min_edge[1], min_edge[0]))
+        path.append(min_edge[1])
+        v = min_edge[1]
+    return path
 
 if __name__ == "__main__":
 
@@ -109,7 +137,9 @@ if __name__ == "__main__":
     op_network = Graph(op_graph)
     
     #Run shortest path algorithm
-    shortest_path(op_network, (10.0, 3.0), (1.0, 8.0))
+    port = (4.5, 5.0)
+    ship = (10.0, 5.5)
+    shortest = shortest_path(op_network, ship, port)
 
     #Draw the obstacles and operating network
     fig = plt.figure(1, figsize=(5,5), dpi=90)
@@ -134,6 +164,13 @@ if __name__ == "__main__":
             y.append(vertex[1])
         ax.plot(x, y, color='red', alpha=0.7, linewidth=1)
 
+    #draw shortest path
+    x = []
+    y = []
+    for vertex in shortest:
+        x.append(vertex[0])
+        y.append(vertex[1])
+        ax.plot(x, y, color='green', alpha=0.7, linewidth=2)
     
     ax.set_title("ocean shortest path test")
     xrange = [0, 11]
