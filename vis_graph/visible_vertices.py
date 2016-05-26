dfd96d5baf15b0138fba97a6a6b254318a124929
Christian Reksten-Monsen <christian@reksten-monsen.com>
2016-05-26 17:33:25 +0800

    clean comments, PEP8 refactor

--- vis_graph/visible_vertices.py
+++ vis_graph/visible_vertices.py
@@ -26,24 +26,26 @@ from math import pi, sqrt, atan, acos
 from graph import Point, Edge, Graph
 
 
-# TODO: remove ship, port, this should be in visibility_graph in a new file
-def visible_vertices(point, graph, ship, port):
+def visible_vertices(point, graph, origin=None, destination=None):
     """Returns list of Points in 'graph' visible by 'point'.
+
+    Args:
+        point: Point to check visibility from.
+        graph: Graph to check visibility against.
+        origin: A optional origin/starting Point included in visibility test
+        destination: A optional destination Point included in visibility test
     """
     edges = graph.get_edges()
     points = graph.get_points()
-    points.append(ship)
-    if port:
-        points.append(port)
+    if origin and origin != point: points.append(origin)
+    if destination and destination != point: points.append(destination)
     points.remove(point)
-
-    # Sort points by angle from x-axis with point as center. If angle is same,
-    # sort by point closest to center.
-    # TODO: Check list.sort() runtime, need O(nlogn)
     points.sort(key=lambda p: (angle(point, p), edge_distance(point, p)))
 
-    ''' Initialize open_edges list with any intersecting edges from point to
-    the first point in angle sorted point list.'''
+    # Initialize open_edges list with any intersecting edges from point to
+    # the first point in angle sorted point list.
+    # TODO: Implement AVL tree for O(logn) insert, delete, search, which is
+    # better than list sort O(nlogn), delete O(n).
     open_edges = []
     for edge in edges:
         if edge_intersect(point, points[0], edge):
@@ -51,8 +53,7 @@ def visible_vertices(point, graph, ship, port):
     open_edges.sort(key=lambda e: point_edge_distance(point, points[0], edge))
 
     visible = []
-    previous_point = None
-    ''' Visit all points in graph to check visibility from point. '''
+    prev_point = None
     for p in points:
         for edge in graph[p]:
                 try:
@@ -60,30 +61,33 @@ def visible_vertices(point, graph, ship, port):
                 except ValueError:
                     pass
         is_visible = False
-        ''' Not ideal, checking each open_edge, but should be able to add new
-        open edges in sorted order, so you only check the closest edge.
-        TODO: Implement AVL-Tree'''
+
+        # HACK: When a AVL tree is implemented and open_edges is in sorted
+        # order, will only need to check open_edges[0]
         smallest_edge = float('inf')
-        for e in open_edges:
-            dist = point_edge_distance(point, p, e)
+        for edge in open_edges:
+            dist = point_edge_distance(point, p, edge)
             if dist < smallest_edge:
                 smallest_edge = dist
+
         if not open_edges or edge_distance(point, p) <= smallest_edge:
-            if previous_point is not None and angle(point, p) == angle(point, previous_point):
-                if edge_distance(point, p) < edge_distance(point, previous_point):
+            if prev_point and angle(point, p) == angle(point, prev_point):
+                if edge_distance(point, p) < edge_distance(point, prev_point):
                     is_visible = True
             else:
                 is_visible = True
-        ''' Check that visibility is not through a polygon.
-        TODO: If polygon is convex, it is simple to check pip().
-        http://stackoverflow.com/questions/471962/how-do-determine-if-a-polygon-is-complex-convex-nonconvex'''
-        if is_visible and p.polygon_id == point.polygon_id and p not in graph.get_adjacent_points(point):
-            is_visible = point_in_polygon(point, p, graph)
 
-        if is_visible:
-            visible.append(p)
+        # Check that visibility is not through a polygon.
+        # TODO: In Graph, add a attribute that states if a polygon is convex
+        # or not (http://bit.ly/1RsvqpO). If polygon is convex, it is simple
+        # to check if point in polygon.
+        if is_visible and p.polygon_id == point.polygon_id:
+            if p not in graph.get_adjacent_points(point):
+                is_visible = point_in_polygon(point, p, graph)
 
-        ''' Add edges in order TODO: add into AVL-Tree '''
+        if is_visible: visible.append(p)
+
+        # TODO: Add edges, if any, into AVL-Tree when implemented
         edge_order = []
         for edge in graph[p]:
             if (point not in edge) and ccw(point, p, edge.get_adjacent(p)):
@@ -91,18 +95,18 @@ def visible_vertices(point, graph, ship, port):
         edge_order.sort(key=lambda x: x[0])
         for e in edge_order:
             open_edges.append(e[1])
-        previous_point = p
+        prev_point = p
 
     return visible
 
 
 def point_in_polygon(p1, p2, graph):
-    mid_point = Point((p1.x+p2.x)/2, (p1.y+p2.y)/2)
+    mid_point = Point((p1.x + p2.x) / 2, (p1.y + p2.y) / 2)
     mid_point_end = Point(float('inf'), mid_point.y)
     intersect_count = 0
     for edge in graph.get_edges():
-        ''' TODO: When make a Polygon class, must pull edges from that and not
-        from the whole graph as below '''
+        # TODO: Implement a Polygon class. Pull edges from that and not
+        # from the whole graph as below.
         if edge.points[0].polygon_id == p1.polygon_id:
             if edge_intersect(mid_point, mid_point_end, edge):
                 intersect_count += 1
@@ -111,48 +115,38 @@ def point_in_polygon(p1, p2, graph):
     return False
 
 
-def angle2(point_a, point_b, point_c):
-    a = edge_distance(point_b, point_c)
-    b = edge_distance(point_a, point_c)
-    c = edge_distance(point_a, point_b)
-    return acos((a**2 + c**2 - b**2) / (2*a*c))
+def edge_distance(p1, p2):
+    """Return the Euclidean distance between two Points."""
+    return sqrt((p2.x - p1.x)**2 + (p2.y - p1.y)**2)
 
 
-def edge_distance(point1, point2):
-    """
-    Return the Euclidean distance between two Points.
-    """
-    return sqrt((point2.x - point1.x)**2 + (point2.y - point1.y)**2)
-
-
-def point_edge_distance(point1, point2, edge):
-    """
-    The line going from point1 to point2, intersects edge before reaching
+def point_edge_distance(p1, p2, edge):
+    """The line going from point1 to point2, intersects edge before reaching
     point2. Return the distance from point1 to this interect point.
     """
-    edge_point1, edge_point2 = edge.points
+    edge_p1, edge_p2 = edge.points
 
-    if edge_point1.x == edge_point2.x:
-        if point1.x == point2.x:
+    if edge_p1.x == edge_p2.x:
+        if p1.x == p2.x:
             return 0
-        points_slope = (point1.y - point2.y) / (point1.x - point2.x)
-        intersect_x = edge_point1.x
-        intersect_y = points_slope * (intersect_x - point1.x) + point1.y
-        return edge_distance(point1, Point(intersect_x, intersect_y))
+        pslope = (p1.y - p2.y) / (p1.x - p2.x)
+        intersect_x = edge_p1.x
+        intersect_y = pslope * (intersect_x - p1.x) + p1.y
+        return edge_distance(p1, Point(intersect_x, intersect_y))
 
-    if point1.x == point2.x:
-        edge_slope = (edge_point1.y - edge_point2.y) / (edge_point1.x - edge_point2.x)
-        intersect_x = point1.x
-        intersect_y = edge_slope * (intersect_x - edge_point1.x) + edge_point1.y
-        return edge_distance(point1, Point(intersect_x, intersect_y))
+    if p1.x == p2.x:
+        eslope = (edge_p1.y - edge_p2.y) / (edge_p1.x - edge_p2.x)
+        intersect_x = p1.x
+        intersect_y = eslope * (intersect_x - edge_p1.x) + edge_p1.y
+        return edge_distance(p1, Point(intersect_x, intersect_y))
 
-    points_slope = (point1.y - point2.y) / (point1.x - point2.x)
-    edge_slope = (edge_point1.y - edge_point2.y) / (edge_point1.x - edge_point2.x)
-    if edge_slope == points_slope:
+    pslope = (p1.y - p2.y) / (p1.x - p2.x)
+    eslope = (edge_p1.y - edge_p2.y) / (edge_p1.x - edge_p2.x)
+    if eslope == pslope:
         return 0
-    intersect_x = (edge_slope*edge_point1.x - points_slope*point1.x + point1.y - edge_point1.y) / (edge_slope - points_slope)
-    intersect_y = edge_slope * (intersect_x - edge_point1.x) + edge_point1.y
-    return edge_distance(point1, Point(intersect_x, intersect_y))
+    intersect_x = (eslope * edge_p1.x - pslope * p1.x + p1.y - edge_p1.y) / (eslope - pslope)
+    intersect_y = eslope * (intersect_x - edge_p1.x) + edge_p1.y
+    return edge_distance(p1, Point(intersect_x, intersect_y))
 
 
 def angle(center, point):
@@ -161,32 +155,36 @@ def angle(center, point):
     dy = point.y - center.y
     if dx == 0:
         if dy < 0:
-            return pi*3 / 2
+            return pi * 3 / 2
         return pi / 2
     if dy == 0:
         if dx < 0:
             return pi
         return 0
     if dx < 0:
-        return pi + atan(dy/dx)
+        return pi + atan(dy / dx)
     if dy < 0:
-        return 2*pi + atan(dy/dx)
-    return atan(dy/dx)
+        return 2 * pi + atan(dy / dx)
+    return atan(dy / dx)
+
+
+def angle2(point_a, point_b, point_c):
+    a = edge_distance(point_b, point_c)
+    b = edge_distance(point_a, point_c)
+    c = edge_distance(point_a, point_b)
+    return acos((a**2 + c**2 - b**2) / (2 * a * c))
 
 
 def ccw(A, B, C):
-    return (C.y-A.y) * (B.x-A.x) > (B.y-A.y) * (C.x-A.x)
+    return (C.y - A.y) * (B.x - A.x) > (B.y - A.y) * (C.x - A.x)
 
 
 def edge_intersect(A, B, edge):
-    """
-    Return True if 'edge' is interesected by the line going through 'A' and
+    """Return True if 'edge' is interesected by the line going through 'A' and
     'B', False otherwise. If edge contains either 'A' or 'B', return False.
-    TODO: May be an issue with colinerity or intersections at vertex points.
-    See:
-    http://www.geeksforgeeks.org/check-if-two-given-line-segments-intersect/
     """
+    # TODO: May be an issue with colinerity or intersections at vertex points.
+    # http://www.geeksforgeeks.org/check-if-two-given-line-segments-intersect/
     C, D = edge.points
-    if A in edge or B in edge:
-        return False
+    if A in edge or B in edge: return False
     return ccw(A, C, D) != ccw(B, C, D) and ccw(A, B, C) != ccw(A, B, D)
