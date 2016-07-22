bf2b47e69a30e2972b5cf947fbe1b5381336f6ea
Christian Reksten-Monsen <christian@reksten-monsen.com>
2016-07-22 16:20:30 +0800

    Change test_country.py with performance timings and also test for adding origin/destination post fact.

--- examples/test_country.py
+++ examples/test_country.py
@@ -23,51 +23,70 @@ SOFTWARE.
 """
 from __future__ import division
 import shapefile
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+import time
 from vis_graph.graph import Graph, Point, Edge
 from vis_graph.shortest_path import shortest_path
-from vis_graph.vis_graph import vis_graph
+from vis_graph.vis_graph import vis_graph, visible_vertices
 
 
 def test_country():
     sf = shapefile.Reader("examples/shapefiles/GSHHS_c_L1.dbf")
     shapes = sf.shapes()
-    poly1 = [Point(a[0], a[1]) for a in shapes[4].points]
+    polys = []
+    # was shapes[4]
+    for i in range(4, 5):
+        polys.append([Point(a[0], a[1]) for a in shapes[i].points])
+    graph = Graph(polys)
+    print "Graph points: {} edges: {}".format(len(graph.graph), len(graph.get_edges()))
 
-    graph = Graph([poly1])
-    ship = Point(-50, -40)
-    port = Point(-50, 10)
-    op_net = vis_graph(graph, ship, port)
-    print "Operating network size: {}".format(len(op_net.get_edges()))
-    '''
+    t0 = time.clock()
+    op_net = vis_graph(graph)
+    t1 = time.clock()
+    print "Time to create visibility graph: {}".format(t1 - t0)
+
+    t2 = time.clock()
+    origin = Point(-50, -40)
+    destination = Point(-50, 10)
+    for v in visible_vertices(origin, graph, destination=destination):
+        op_net.add_edge(Edge(origin, v))
+    for v in visible_vertices(destination, graph, origin=origin):
+        op_net.add_edge(Edge(destination, v))
+    t3 = time.clock()
+    print "Time to update vis graph with origin/dest: {}".format(t3 - t2)
+    print "visibility graph edges: {}".format(len(op_net.get_edges()))
+
+    t4 = time.clock()
+    shortest = shortest_path(op_net, origin, destination)
+    t5 = time.clock()
+    print "Shorest path hops: {}".format(len(shortest))
+    print "Time to calculate shortest path: {}".format(t5 - t4)
+
+    t6 = time.clock()
     fig = plt.figure(1, figsize=(10, 10), dpi=90)
     ax = fig.add_subplot(111)
-
     # Draw the country
-    x = [a.x for a in poly1]
-    y = [a.y for a in poly1]
-    ax.plot(x, y, color='black', alpha=0.7, linewidth=1,
-            solid_capstyle='round', zorder=2)
-
+    for poly in polys:
+        x = [a.x for a in poly]
+        y = [a.y for a in poly]
+        ax.plot(x, y, color='black', alpha=0.7, linewidth=1,
+                solid_capstyle='round', zorder=2)
     # Draw the visibility graph
     for e in op_net.get_edges():
         x = [e.points[0].x, e.points[1].x]
         y = [e.points[0].y, e.points[1].y]
         ax.plot(x, y, color='red', alpha=0.5, linewidth=0.5)
-
-    # Hacky test of shortest path
-    shortest = shortest_path(op_net, ship, port)
-    print "Shorest path hops: {}".format(len(shortest))
     x = [v.x for v in shortest]
     y = [v.y for v in shortest]
     # Draw the shortest path
     ax.plot(x, y, color='green', alpha=0.7, linewidth=2.0)
-
+    # Annotate
     ax.set_title("Python visibility graph & shortest path")
-    ax.annotate('Origin', xytext=(ship.x, ship.y), xy=(ship.x, ship.y))
-    ax.annotate('Destination', xytext=(port.x, port.y), xy=(port.x, port.y))
+    ax.annotate('Origin', xytext=(origin.x, origin.y), xy=(origin.x, origin.y))
+    ax.annotate('Destination', xytext=(destination.x, destination.y), xy=(destination.x, destination.y))
     fig.savefig("poly.png", bbox_inches='tight')
-    '''
+    t7 = time.clock()
+    print "Time to draw graph: {}".format(t7 - t6)
 
 if __name__ == "__main__":
     test_country()
