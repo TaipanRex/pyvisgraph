fce126c84a357f9454839470373c949c16828979
Christian Reksten-Monsen <christian@reksten-monsen.com>
2016-09-23 18:04:00 +0800

    Rewrite shortest_path to workable implementation, using priority dict.

---
+++ utils/priority_dict.py
@@ -0,0 +1,89 @@
+"""
+The MIT License (MIT)
+
+Copyright (c) 2016 Christian August Reksten-Monsen
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+from heapq import heapify, heappush, heappop
+
+
+class priority_dict(dict):
+    """Dictionary that can be used as a priority queue.
+
+    Keys of the dictionary are items to be put into the queue, and values
+    are their respective priorities. All dictionary methods work as expected.
+    The advantage over a standard heapq-based priority queue is that priorities
+    of items can be efficiently updated (amortized O(1)) using code as
+    'thedict[item] = new_priority.'
+
+    Note that this is a modified version of
+    https://gist.github.com/matteodellamico/4451520 where sorted_iter() has
+    been replaced with the destructive sorted iterator __iter__ from
+    https://gist.github.com/anonymous/4435950
+    """
+    def __init__(self, *args, **kwargs):
+        super(priority_dict, self).__init__(*args, **kwargs)
+        self._rebuild_heap()
+
+    def _rebuild_heap(self):
+        self._heap = [(v, k) for k, v in self.iteritems()]
+        heapify(self._heap)
+
+    def smallest(self):
+        heap = self._heap
+        v, k = heap[0]
+        while k not in self or self[k] != v:
+            heappop(heap)
+            v, k = heap[0]
+        return k
+
+    def pop_smallest(self):
+        heap = self._heap
+        v, k = heappop(heap)
+        while k not in self or self[k] != v:
+            v, k = heappop(heap)
+        del self[k]
+        return k
+
+    def __setitem__(self, key, val):
+        super(priority_dict, self).__setitem__(key, val)
+
+        if len(self._heap) < 2 * len(self):
+            heappush(self._heap, (val, key))
+        else:
+            self._rebuild_heap()
+
+    def setdefault(self, key, val):
+        if key not in self:
+            self[key] = val
+            return val
+        return self[key]
+
+    def update(self, *args, **kwargs):
+        super(priority_dict, self).update(*args, **kwargs)
+        self._rebuild_heap()
+
+    def __iter__(self):
+        def iterfn():
+            while len(self) > 0:
+                x = self.smallest()
+                yield x
+                del self[x]
+        return iterfn()
---
+++ utils/shortest_path.py
@@ -0,0 +1,59 @@
+"""
+The MIT License (MIT)
+
+Copyright (c) 2016 Christian August Reksten-Monsen
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+from vis_graph.graph import Point, Edge, Graph
+from vis_graph.visible_vertices import edge_distance
+from utils.priority_dict import priority_dict
+
+
+def dijkstra(graph, origin, destination=None):
+    D = {}
+    P = {}
+    Q = priority_dict()
+    Q[origin] = 0
+
+    for v in Q:
+        D[v] = Q[v]
+        if v == destination: break
+
+        for e in graph[v]:
+            w = e.get_adjacent(v)
+            elength = D[v] + edge_distance(v, w)
+            if w in D:
+                if elength < D[w]:
+                    raise ValueError
+            elif w not in Q or elength < Q[w]:
+                Q[w] = elength
+                P[w] = v
+    return (D, P)
+
+
+def shortest_path(graph, origin, destination):
+    D, P = dijkstra(graph, origin, destination)
+    path = []
+    while 1:
+        path.append(destination)
+        if destination == origin: break
+        destination = P[destination]
+    path.reverse()
+    return path
