20bf79ef376c99e5c665b9cfdd69de605de7f8a0
Christian Reksten-Monsen <christian@reksten-monsen.com>
2016-09-29 19:56:43 +0800

    minor

--- examples/test_country.py
+++ examples/test_country.py
@@ -23,12 +23,15 @@ SOFTWARE.
 """
 from __future__ import division
 import shapefile
-import matplotlib.pyplot as plt
 import time
 import pickle
 from vis_graph.graph import Graph, Point, Edge
 from vis_graph.vis_graph import vis_graph, visible_vertices
 from utils.shortest_path import shortest_path
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
 
 def test_country():
     #sf = shapefile.Reader("examples/shapefiles/ne_110m_coastline.shp")
@@ -36,8 +39,15 @@ def test_country():
     shapes = sf.shapes()
     polys = []
 
-    #polys.append([Point(a[0], a[1]) for a in shapes[5].points])
-
+    #polys = [[Point(1,2)],[Point(3,4),Point(4,3),Point(3.5,4),Point(4,5.5),Point(3,4)],[Point(6,8),Point(7,1),Point(8,9),Point(7,3),Point(6,8)]]#[Point(7,5),Point(7.2,6),Point(7,5.5),Point(6.8,6),Point(7,5)]
+    #polys.append([Point(a[0], a[1]) for a in shapes[4].points])
+    '''
+    for a in shapes[10].points:
+        p = Point(a[0], a[1])
+        if p.x<-79.0 and p.y >71.50 and p.y < 73.7:
+            polys.append(p)
+    polys = [polys]
+    '''
     for shape in shapes:
         poly = []
         if len(shape.points) > 4:
@@ -45,21 +55,20 @@ def test_country():
                 p = Point(a[0], a[1])
                 poly.append(p)
             polys.append(poly)
-
     graph = Graph(polys)
     print "Graph points: {} edges: {}".format(len(graph.graph), len(graph.get_edges()))
 
     t0 = time.clock()
     op_net = None
-    with open('visgraph.pk1', 'rb') as load:
-        op_net = pickle.load(load)
-    #op_net = vis_graph(graph, init_depth=3)
+    #with open('visgraph.pk1', 'rb') as load:
+    #    op_net = pickle.load(load)
+    op_net = vis_graph(graph)
     t1 = time.clock()
     print "Time to create visibility graph: {}".format(t1 - t0)
-
-    #with open('visgraph.pk1', 'wb') as output:
-    #    pickle.dump(op_net, output, -1)
-
+    print "visibility graph edges: {}".format(len(op_net.get_edges()))
+    with open('visgraph.pk1', 'wb') as output:
+        pickle.dump(op_net, output, -1)
+    '''
     t2 = time.clock()
     origin = Point(60.0, 0.0)
     destination = Point(40.0, 70.0)
@@ -76,7 +85,7 @@ def test_country():
     t5 = time.clock()
     print "Shorest path hops: {}".format(len(shortest))
     print "Time to calculate shortest path: {}".format(t5 - t4)
-
+    '''
     t6 = time.clock()
     fig = plt.figure(1, figsize=(30, 30), dpi=90)
     ax = fig.add_subplot(111)
@@ -88,18 +97,18 @@ def test_country():
                 solid_capstyle='round', zorder=2)
 
     # Draw the visibility graph
-    #for e in op_net.get_edges():
-    #    x = [e.points[0].x, e.points[1].x]
-    #    y = [e.points[0].y, e.points[1].y]
-    #    ax.plot(x, y, color='red', alpha=0.5, linewidth=0.5)
-    x = [v.x for v in shortest]
-    y = [v.y for v in shortest]
+    for e in op_net.get_edges():
+        x = [e.points[0].x, e.points[1].x]
+        y = [e.points[0].y, e.points[1].y]
+        ax.plot(x, y, color='red', alpha=0.5, linewidth=0.5)
+    #x = [v.x for v in shortest]
+    #y = [v.y for v in shortest]
     # Draw the shortest path
-    ax.plot(x, y, color='green', alpha=0.7, linewidth=2.0)
+    #ax.plot(x, y, color='green', alpha=0.7, linewidth=2.0)
     # Annotate
     ax.set_title("Python visibility graph & shortest path")
-    ax.annotate('Origin', xytext=(origin.x, origin.y), xy=(origin.x, origin.y))
-    ax.annotate('Destination', xytext=(destination.x, destination.y), xy=(destination.x, destination.y))
+    #ax.annotate('Origin', xytext=(origin.x, origin.y), xy=(origin.x, origin.y))
+    a#x.annotate('Destination', xytext=(destination.x, destination.y), xy=(destination.x, destination.y))
     fig.savefig("poly.png", bbox_inches='tight')
     t7 = time.clock()
     print "Time to draw graph: {}".format(t7 - t6)
