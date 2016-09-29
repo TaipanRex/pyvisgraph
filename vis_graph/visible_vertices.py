5407235bd382968f72329a98f209f67b9f7d94a5
Christian Reksten-Monsen <christian@reksten-monsen.com>
2016-09-29 19:52:45 +0800

    Huge commit! AVL tree working with open_edges and the many changes necessary to make it work.

--- vis_graph/visible_vertices.py
+++ vis_graph/visible_vertices.py
@@ -24,9 +24,10 @@ SOFTWARE.
 from __future__ import division
 from math import pi, sqrt, atan, acos
 from graph import Point, Edge, Graph
+from utils.avl import AVLTree
 
 
-def visible_vertices(point, graph, origin=None, destination=None, init_depth=1):
+def visible_vertices(point, graph, origin=None, destination=None):
     """Returns list of Points in 'graph' visible by 'point'.
 
     Args:
@@ -43,37 +44,29 @@ def visible_vertices(point, graph, origin=None, destination=None, init_depth=1):
 
     # Initialize open_edges list with any intersecting edges from point to
     # the first point in angle sorted point list.
-    # TODO: Implement AVL tree for O(logn) insert, delete, search, which is
-    # better than list sort O(nlogn), delete O(n).
-    open_edges = []
-    for edge in edges:
-        # TODO: Maybe instead of doing this, set the sweep line to point, p2.x = inf, p2.y = point.y!
-        for i in range(0, init_depth):
-            if edge_intersect(point, points[i], edge):
-                open_edges.append(edge)
-    # TODO: What if 2 collinear edges?
-    open_edges.sort(key=lambda e: point_edge_distance(point, points[0], edge))
+    open_edges = AVLTree()
+    point_inf = Point(10000.0, point.y)
+    for e in edges:
+        if point in e:
+            continue
+        if edge_intersect(point, point_inf, e):
+            i = intersect_point(point, point_inf, e)
+            if e.points[0].y > i.y or e.points[1].y > i.y:
+                k = EdgeKey(point, point_inf, e)
+                open_edges.insert(k)
 
     visible = []
     prev_point = None
     for p in points:
         if p == point: continue
         for edge in graph[p]:
-                try:
-                    open_edges.remove(edge)
-                except ValueError:
-                    pass
+            if ccw(point, p, edge.get_adjacent(p)) == -1:
+                k = EdgeKey(point, p, edge)
+                open_edges.delete(k)
         is_visible = False
 
-        # HACK: When a AVL tree is implemented and open_edges is in sorted
-        # order, will only need to check open_edges[0]
-        smallest_edge = float('inf')
-        for edge in open_edges:
-            dist = point_edge_distance(point, p, edge)
-            if dist < smallest_edge:
-                smallest_edge = dist
-
-        if not open_edges or edge_distance(point, p) <= smallest_edge:
+        smallest_edge = open_edges.smallest()
+        if not smallest_edge or edge_distance(point, p) <= point_edge_distance(point, p, smallest_edge.edge):
             if prev_point and angle(point, p) == angle(point, prev_point):
                 if edge_distance(point, p) < edge_distance(point, prev_point):
                     is_visible = True
@@ -89,15 +82,11 @@ def visible_vertices(point, graph, origin=None, destination=None, init_depth=1):
                 is_visible = point_in_polygon(point, p, graph.get_edges())
 
         if is_visible: visible.append(p)
-
-        # TODO: Add edges, if any, into AVL-Tree when implemented
-        edge_order = []
         for edge in graph[p]:
             if (point not in edge) and ccw(point, p, edge.get_adjacent(p)) == 1:
-                edge_order.append((angle2(point, p, edge.get_adjacent(p)), edge))
-        edge_order.sort(key=lambda x: x[0])
-        for e in edge_order:
-            open_edges.append(e[1])
+                k = EdgeKey(point, p, edge)
+                open_edges.insert(k)
+
         prev_point = p
 
     return visible
@@ -113,11 +102,12 @@ def point_in_polygon(p1, p2, graph_edges):
         # TODO: Implement a Polygon class. Pull edges from that and not
         # from the whole graph as below.
         if edge.points[0].polygon_id == p1.polygon_id:
+            # collinear points on right side
             co0 = ccw(mid_p1, edge.points[0], mid_p2) == 0 and edge.points[0].x > mid_p1.x
             co1 = ccw(mid_p1, edge.points[1], mid_p2) == 0 and edge.points[1].x > mid_p1.x
             co_point = edge.points[0] if co0 else edge.points[1]
             if co0 or co1:
-                if co_point.y > mid_p1.y:
+                if edge.get_adjacent(co_point).y > mid_p1.y:
                     co_dir += 1
                 else:
                     co_dir -= 1
@@ -143,29 +133,36 @@ def point_edge_distance(p1, p2, edge):
     """The line going from point1 to point2, intersects edge before reaching
     point2. Return the distance from point1 to this interect point.
     """
+    intersect_p = intersect_point(p1, p2, edge)
+    if intersect_p is not None:
+        return edge_distance(p1, intersect_p)
+    return 0
+
+
+def intersect_point(p1, p2, edge):
     edge_p1, edge_p2 = edge.points
 
     if edge_p1.x == edge_p2.x:
         if p1.x == p2.x:
-            return 0
+            return None
         pslope = (p1.y - p2.y) / (p1.x - p2.x)
         intersect_x = edge_p1.x
         intersect_y = pslope * (intersect_x - p1.x) + p1.y
-        return edge_distance(p1, Point(intersect_x, intersect_y))
+        return Point(intersect_x, intersect_y)
 
     if p1.x == p2.x:
         eslope = (edge_p1.y - edge_p2.y) / (edge_p1.x - edge_p2.x)
         intersect_x = p1.x
         intersect_y = eslope * (intersect_x - edge_p1.x) + edge_p1.y
-        return edge_distance(p1, Point(intersect_x, intersect_y))
+        return Point(intersect_x, intersect_y)
 
     pslope = (p1.y - p2.y) / (p1.x - p2.x)
     eslope = (edge_p1.y - edge_p2.y) / (edge_p1.x - edge_p2.x)
     if eslope == pslope:
-        return 0
+        return None
     intersect_x = (eslope * edge_p1.x - pslope * p1.x + p1.y - edge_p1.y) / (eslope - pslope)
     intersect_y = eslope * (intersect_x - edge_p1.x) + edge_p1.y
-    return edge_distance(p1, Point(intersect_x, intersect_y))
+    return Point(intersect_x, intersect_y)
 
 
 def angle(center, point):
@@ -191,7 +188,7 @@ def angle2(point_a, point_b, point_c):
     a = edge_distance(point_b, point_c)
     b = edge_distance(point_a, point_c)
     c = edge_distance(point_a, point_b)
-    x = round((a**2 + c**2 - b**2) / (2 * a * c), 6)
+    x = (a**2 + c**2 - b**2) / (2 * a * c)
     return acos(x)
 
 
@@ -209,5 +206,35 @@ def edge_intersect(A, B, edge):
     # TODO: May be an issue with colinerity or intersections at vertex points.
     # http://www.geeksforgeeks.org/check-if-two-given-line-segments-intersect/
     C, D = edge.points
-    if A in edge or B in edge: return False
     return ccw(A, C, D) != ccw(B, C, D) and ccw(A, B, C) != ccw(A, B, D)
+
+
+class EdgeKey:
+    def __init__(self, p1, p2, edge):
+        self.p1 = p1
+        self.p2 = p2
+        self.edge = edge
+
+    def __cmp__(self, other):
+        if self.edge == other.edge:
+            return 0
+        # if other p2 is in both edges, distance is equal and we need the angle
+        if other.p2 in self.edge:
+            aslf = angle2(other.p1, other.p2, self.edge.get_adjacent(other.p2))
+            aot = angle2(other.p1, other.p2, other.edge.get_adjacent(other.p2))
+            if aot < aslf:
+                return 1
+            return -1
+        # if no intersect with self, self > other
+        if not edge_intersect(other.p1, other.p2, self.edge):
+            return 1
+        # last case is where we need to recalculate distance for me, other is edge_distance p1,p2
+        self_dist = point_edge_distance(other.p1, other.p2, self.edge)
+        other_dist = point_edge_distance(other.p1, other.p2, other.edge)
+        if self_dist > other_dist:
+            return 1
+        return -1
+
+    def __repr__(self):
+        reprstring = (self.__class__.__name__, self.edge, self.p1, self.p2)
+        return "{}(Edge={!r}, p1={!r}, p2={!r})".format(*reprstring)
