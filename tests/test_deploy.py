d7dc171b06f27f31cff458492e02922eab68b77c
Christian Reksten-Monsen <christian@reksten-monsen.com>
2016-10-26 10:30:11 +0800

    test: added test suite and tox configuration
    Tox will test the test suite using both Python 2 and Python 3.
    Current coverage is around 90%. Note that test_deploy.py requires
    a pre-built pk1 obstacle graph, which will not be pushed.

---
+++ tests/test_deploy.py
@@ -0,0 +1,89 @@
+"""
+The MIT License (MIT)
+
+Copyright (c) 2016 Christian August Reksten-Monsen
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+import shapefile
+import pyvisgraph as vg
+
+class TestVisGraphBuild:
+
+    def setup_method(self, method):
+        self.sf = shapefile.Reader("examples/shapefiles/GSHHS_c_L1.dbf")
+        self.shapes = self.sf.shapes()
+        self.polys = []
+        self.polys.append([vg.Point(a[0], a[1]) for a in self.shapes[4].points])
+
+
+    def test_build_1_core(self):
+        world = vg.VisGraph()
+        world.build(self.polys)
+        assert len(world.graph.get_points()) == 310
+        assert len(world.graph.get_edges()) == 310
+        assert len(world.visgraph.get_edges()) == 1156
+        s = "Graph points: {} edges: {}, visgraph edges: {}"
+        print(s.format(len(world.graph.get_points()),
+                       len(world.graph.get_edges()),
+                       len(world.visgraph.get_edges())))
+
+    def test_build_2_core(self):
+        world = vg.VisGraph()
+        world.build(self.polys, workers=2)
+        assert len(world.graph.get_points()) == 310
+        assert len(world.graph.get_edges()) == 310
+        assert len(world.visgraph.get_edges()) == 1156
+        s = "Graph points: {} edges: {}, visgraph edges: {}"
+        print(s.format(len(world.graph.get_points()),
+                       len(world.graph.get_edges()),
+                       len(world.visgraph.get_edges())))
+
+
+class TestVisGraphMethods:
+
+    def setup_method(self, method):
+        self.world = vg.VisGraph()
+        self.world.load('world.pk1')
+        self.origin = vg.Point(100, -20)
+        self.destination = vg.Point(25, 75)
+
+    def test_shortest_path(self):
+        shortest = self.world.shortest_path(self.origin, self.destination)
+        assert len(shortest) == 19
+
+    def test_shortest_path_not_update_visgraph(self):
+        shortest = self.world.shortest_path(self.origin, self.destination)
+        assert self.origin not in world.visgraph
+        assert self.destination not in world.visgraph
+
+    def test_shortest_path_not_update_graph(self):
+        shortest = self.world.shortest_path(self.origin, self.destination)
+        assert self.origin not in world.graph
+        assert self.destination not in world.graph
+
+    def test_update(self):
+        self.world.update([self.origin, self.destination])
+        assert self.origin in self.world.visgraph
+        assert self.destination in self.world.visgraph
+
+    def test_update_not_update_graph(self):
+        self.world.update([self.origin, self.destination])
+        assert self.origin not in self.world.graph
+        assert self.destination not in self.world.graph
---
+++ tests/test_pvg.py
@@ -0,0 +1,192 @@
+"""
+The MIT License (MIT)
+
+Copyright (c) 2016 Christian August Reksten-Monsen
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+from __future__ import division
+from pyvisgraph.graph import Graph, Point, Edge
+from pyvisgraph.visible_vertices import edge_intersect, point_edge_distance
+from pyvisgraph.visible_vertices import visible_vertices, angle
+from pyvisgraph.visible_vertices import intersect_point, edge_distance
+from math import pi, degrees
+import pyvisgraph as vg
+
+'''
+setup_module(module): only run once when file is executed
+teardown_module(module):
+setup_function(function):
+
+In a class:
+setup(self): gets run last when a method is called
+setup_class(cls):
+setup_method(self, method):
+def test_subscribe_when_already_registered():
+    # GIVEN a user is already subscribed to a newsletter
+    # WHEN a user subscribes to the newsletter
+    # THEN no changes are made to the email list
+    # and the user is told they already are subscribed
+'''
+
+
+class TestUndirectedGraph:
+
+    def setup_method(self, method):
+        self.point_a = Point(0.0, 1.0)
+        self.point_b = Point(1.0, 2.0)
+        self.point_c = Point(0.0, 1.0)
+        self.point_d = Point(1.0, 2.0)
+        self.edge_a = Edge(self.point_a, self.point_b)
+        self.edge_b = Edge(self.point_b, self.point_a)
+        self.edge_c = Edge(self.point_c, self.point_d)
+        self.edge_d = Edge(self.point_d, self.point_c)
+
+    def test_point_equality(self):
+        assert self.point_a == Point(0.0, 1.0)
+        assert self.point_a != Point(1.0, 0.0)
+        assert self.edge_a == self.edge_b
+        assert self.edge_a == self.edge_a
+        assert self.edge_a == self.edge_c
+        assert self.edge_a == self.edge_d
+
+
+def test_angle_function():
+    center = Point(1.0, 1.0)
+    point_a = Point(3.0, 1.0)
+    point_b = Point(1.0, 0)
+    point_c = Point(0.0, 2.0)
+    point_d = Point(2.0, 2.0)
+    point_e = Point(2.0, 0.0)
+    point_f = Point(0.0, 0.0)
+    assert angle(center, point_a) == 0
+    assert angle(center, point_b) == pi * 3 / 2
+    assert degrees(angle(center, point_c)) == 135
+    assert degrees(angle(center, point_d)) == 45
+    assert degrees(angle(center, point_e)) == 315
+    assert degrees(angle(center, point_f)) == 225
+
+
+def test_edge_intersect_function():
+    point_a = Point(3.0, 5.0)
+    point_b = Point(5.0, 3.0)
+    point_c = Point(4.0, 2.0)
+    point_d = Point(4.0, 5.0)
+    point_e = Point(5.0, 4.0)
+    point_f = Point(3.0, 4.0)
+    point_g = Point(4.0, 1.0)
+    point_h = Point(6.0, 4.0)
+    point_i = Point(4.0, 4.0)
+    edge = Edge(point_a, point_b)
+    assert edge_intersect(point_c, point_d, edge) is True
+    assert edge_intersect(point_c, point_e, edge) is True
+    assert edge_intersect(point_f, point_e, edge) is True
+    assert edge_intersect(point_g, point_b, edge) is True
+    assert edge_intersect(point_c, point_h, edge) is True
+    assert edge_intersect(point_h, point_i, edge) is True
+
+
+def test_point_edge_distance_function():
+    point_a = Point(3.0, 1.0)
+    point_b = Point(3.0, 5.0)
+    point_c = Point(2.0, 2.0)
+    point_d = Point(4.0, 4.0)
+    point_e = Point(1.0, 1.0)
+    point_f = Point(1.0, 2.0)
+    point_g = Point(3.0, 4.0)
+    point_h = Point(2.0, 5.0)
+    edge = Edge(point_a, point_b)
+    edge2 = Edge(point_c, point_d)
+    edge3 = Edge(point_e, point_b)
+    assert point_edge_distance(point_c, point_d, edge) == 1.4142135623730951
+    assert point_edge_distance(point_a, point_b, edge2) == 2.0
+    assert point_edge_distance(point_f, point_g, edge3) == 1.4142135623730951
+    assert point_edge_distance(point_h, point_g, edge3) == 0.9428090415820635
+
+def test_point_in_polygon():
+    g = vg.VisGraph()
+    point_a = Point(0,0)
+    point_b = Point(4,0)
+    point_c = Point(2,4)
+    point_d = Point(1,0.5)
+    g.build([[point_a, point_b, point_c]])
+    assert g.point_in_polygon(point_d) != -1
+
+class TestClosestPoint:
+
+    def setup_method(self, method):
+        self.g = vg.VisGraph()
+        self.point_a = Point(0,0)
+        self.point_b = Point(4,0)
+        self.point_c = Point(2,4)
+        self.point_d = Point(1,0.5)
+        self.g.build([[self.point_a, self.point_b, self.point_c]])
+
+    def test_closest_point(self):
+        pid = self.g.point_in_polygon(self.point_d)
+        cp = self.g.closest_point(self.point_d, pid)
+        assert self.g.point_in_polygon(cp) == -1
+
+    def test_closest_point_length(self):
+        pid = self.g.point_in_polygon(self.point_d)
+        cp = self.g.closest_point(self.point_d, pid, length=0.5)
+        ip = intersect_point(self.point_d, cp, Edge(self.point_a, self.point_b))
+        assert edge_distance(ip, cp) == 0.5
+
+    def test_closest_point_edge_point(self):
+        """Test where the cp is a end-point of a polygon edge. Can end up with
+        cp extending into polygon instead of outside it."""
+        g = vg.VisGraph()
+        g.build([[Point(0,1), Point(2,0), Point(1,1), Point(2,2)]])
+        p = Point(1,0.9)
+        pid = g.point_in_polygon(p)
+        cp = g.closest_point(p, pid, length=0.001)
+        print(cp)
+        assert g.point_in_polygon(cp) == -1
+
+class TestCollinear:
+
+    def setup_method(self, method):
+        self.point_a = Point(0,1)
+        self.point_b = Point(1,0)
+        self.point_c = Point(2,3)
+        self.point_d = Point(3,2)
+        self.point_e = Point(3.5,0.5)
+        self.point_f = Point(4.5,3.5)
+
+    def test_collin1(self):
+        graph = Graph([[self.point_a, self.point_b, self.point_c],
+                       [self.point_d, self.point_e, self.point_f]])
+        visible = visible_vertices(Point(1,4), graph, None, None)
+        assert visible == [self.point_a, self.point_c, self.point_f]
+
+    def test_collin2(self):
+        self.point_g = Point(2,5)
+        self.point_h = Point(3,5)
+        graph = Graph([[self.point_g, self.point_h, self.point_c],
+                       [self.point_d, self.point_e, self.point_f]])
+        visible = visible_vertices(Point(1,4), graph, None, None)
+        assert visible == [self.point_g, self.point_e, self.point_c]
+
+    def test_collin3(self):
+        graph = Graph([[Point(0,1), Point(1,0), Point(2,3)],
+                       [Point(2,2), Point(3.5,5), Point(2.5,2)],
+                       [Point(3,2), Point(3.5,0.5), Point(4.5,3.5)]])
+        visible = visible_vertices(Point(1,4), graph, None, None)
+        assert visible == [Point(3.50, 5.00), Point(0.00, 1.00), Point(2.00, 3.00)]
