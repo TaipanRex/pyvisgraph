a8e6ce79f69080a4b2d1ba943f5ac7c2638b294f
Christian Reksten-Monsen <christian@reksten-monsen.com>
2018-05-27 18:59:51 +0800

    push 0.1.8

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'pyvisgraph',
   packages = ['pyvisgraph'],
-  version = '0.1.7',
+  version = '0.1.8',
   description = 'Given a set of simple obstacle polygons, build a visibility graph and find the shortest path between two points.',
   author = 'Christian Reksten-Monsen',
   author_email = 'christian@reksten-monsen.com',
   url = 'https://github.com/TaipanRex/pyvisgraph',
-  download_url = 'https://github.com/TaipanRex/pyvisgraph/tarball/0.1.7',
+  download_url = 'https://github.com/TaipanRex/pyvisgraph/tarball/0.1.8',
   keywords = ['visibility', 'graph', 'shortest'],
   classifiers = [],
 )
