eaaad8b8a70bfdcd1722f44f94c4e9852193ac8b
emile.zhu <emile.zhu@hotmail.com>
2018-06-04 06:11:07 +0800

    add requirements.txt and MANIFEST.in

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
-from distutils.core import setup
+from setuptools import setup
+
+with open('requirements.txt', 'r') as fr:
+  required = fr.read().splitlines()
+  
 setup(
   name = 'pyvisgraph',
   packages = ['pyvisgraph'],
@@ -8,6 +12,7 @@ setup(
   author_email = 'christian@reksten-monsen.com',
   url = 'https://github.com/TaipanRex/pyvisgraph',
   download_url = 'https://github.com/TaipanRex/pyvisgraph/tarball/0.2.1',
+  install_requires = required,
   keywords = ['visibility', 'graph', 'shortest'],
   classifiers = [],
 )
